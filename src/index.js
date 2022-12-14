3878474c8de2d8493c71cc65a7b9eca38a4c94a6
IgorSprovieri <igsp1@hotmail.com>
2022-12-13 21:28:36 -0300

    create routes for categories, mainly post

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("./db");
+const routesCategories = require("./routes/categories");
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/categories", routesCategories);
+
+app.listen(port, () => {
+  db.connect()
+    .then(() => {
+      console.log("DB connected");
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  console.log(`Example app listening on port ${port}`);
+});
